eb451054a76c10c494b62102d1e58193b709d52e
Alex Zhao <zhaochunqi@users.noreply.github.com>
2016-05-30 01:33:34 +0800

    Update change_my_ip_on_dnspod.sh

--- change_my_ip_on_dnspod.sh
+++ change_my_ip_on_dnspod.sh
@@ -5,6 +5,7 @@ domain_id="" #获取到的domain_id
 record_id="" #获取到的record_id
 sub_domain="" #你要更改的subdomain的地址，如www.xxx.com就写www
 
-my_ip=`curl ifconfig.me`
+#my_ip=`curl ifconfig.me`
+my_ip=`curl ident.me`
 echo "Your IP is ${my_ip} ."
 curl -X POST https://dnsapi.cn/Record.Modify -d "login_token=${login_token}&format=json&domain_id=${domain_id}&record_id=${record_id}&sub_domain=${sub_domain}&value=${my_ip}&record_type=A&record_line=默认"
